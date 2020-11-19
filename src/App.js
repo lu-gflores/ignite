63a4d575f8eacc367e245751a0e26b41aa8fea81
George Flores <georgeflores886@gmail.com>
2020-11-19 09:54:09 -0500

    created global styles and rendered new and popular games on home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, {useEffect} from 'react'
 //components
 import Home from './pages/Home'
-
+import GlobalStyles from './components/GlobalStyles'
 
 function App() {
  
   return (
     <div className="App"> 
+    <GlobalStyles/>
       <Home />
     </div>
   );

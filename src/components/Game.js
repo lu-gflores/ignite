0fdc78e858d361cda0de24137405d8ff063adf86
George Flores <georgeflores886@gmail.com>
2020-11-20 10:25:20 -0500

    resized images

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,7 +4,8 @@ import {motion } from 'framer-motion'
 
 import {useDispatch} from 'react-redux'
 import {loadDetail } from '../actions/detailAction'
-import {Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import {smallImage} from '../util'
 
 const Game = ({name, released, image, id }) => {
 
@@ -18,7 +19,7 @@ const Game = ({name, released, image, id }) => {
             <Link to ={`/game/${id}`}>
                <h3>{name}</h3>
                 <p>{released}</p>
-                <img src={image} alt={name}></img>  
+                <img src={smallImage(image, 640)} alt={name}></img>  
             </Link>
                        
         </StyledGame>

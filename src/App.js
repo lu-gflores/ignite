b73a1af74a651a21ebf89958627b0e289536b52d
George Flores <georgeflores886@gmail.com>
2020-11-22 21:12:30 -0500

    added a nav component for search bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, {useEffect} from 'react'
 //components
 import Home from './pages/Home'
+import Nav from './components/Nav'
 import GlobalStyles from './components/GlobalStyles'
 import { Route } from 'react-router-dom'
+
 
 function App() {
  
   return (
     <div className="App"> 
     <GlobalStyles/>
+    <Nav />
     <Route path={['/game/:id', '/']}>
       <Home />
     </Route>

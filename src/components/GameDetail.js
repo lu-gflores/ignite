cf59d341331f64a01da68235a314c97313c5ee4c
George Flores <georgeflores886@gmail.com>
2020-11-22 21:00:48 -0500

    created function to render stars for rating

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -12,7 +12,8 @@ import nintendo from '../img/nintendo.svg'
 import apple from '../img/apple.svg'
 import steam from '../img/steam.svg'
 import gamepad from '../img/gamepad.svg'
-
+import starFull from '../img/star-full.png'
+import starEmpty from '../img/star-empty.png'
 
 const GameDetail = ({pathId}) => {
     const history = useHistory();
@@ -24,6 +25,20 @@ const GameDetail = ({pathId}) => {
             history.push('/')
         }
     }
+    //stars
+    const getStars = () => {
+        const stars = [];
+        const rating = Math.floor(game.rating);
+        for(let i = 1; i <= 5; i++) {
+            if( i <= rating ){
+                stars.push(<img alt='star' key={i} src={starFull}></img>)
+            } else {
+                stars.push(<img alt='star' key={i} src={starEmpty}></img>)
+            }
+        }
+        return stars
+    }
+
 
     const getPlatform = platform => {
         switch(platform) {
@@ -55,6 +70,7 @@ const GameDetail = ({pathId}) => {
                     <div className="rating">
                         <motion.h3 layoutId={`title ${pathId}`}>{game.name}</motion.h3>
                         <p>Rating: {game.rating}</p>
+                        {getStars()}
                     </div>
                     <Info>
                         <h3>Platforms</h3>
@@ -119,6 +135,11 @@ const Stats = styled(motion.div)`
     display: flex;
     align-items: center;
     justify-content: space-between;
+    img{
+        height: 2rem;
+        width: 2rem;
+        display: inline;
+    }
 `
 const Info = styled(motion.div)`
     text-align: center;
